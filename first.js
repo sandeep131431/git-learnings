5cc3accd347d4a275b7cd2816067f6689bddd28c
Sandeep Kumar <skumar131431@gmail.com>
2025-01-11 11:50:20 +0530

    update payment

--- first.js
+++ first.js
@@ -4,5 +4,10 @@ console.log(button);
 const longin = "log in page added";
 console.log(longin);
 
+<<<<<<< HEAD
 const footer  = "footer added";
 console.log(footer);
+=======
+const pay = "integrated the payment added";
+console.log(pay);
+>>>>>>> d209fad (payment  add)
